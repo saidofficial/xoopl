d935569081aba0077821cd3081a7628706609cdb
Just Said <97128346+saidofficial@users.noreply.github.com>
2022-05-09 21:19:40 +0500

    changes)

--- lib/lexer/mod.rs
+++ lib/lexer/mod.rs
@@ -4,3 +4,12 @@
 
 
 */
+
+use token::Token;
+
+pub struct Lexer<'a> {
+    input: &'a str,
+    pos: usize,
+    next_pos: usize,
+    ch: u8,
+}
--- lib/lexer/token.rs
+++ lib/lexer/token.rs
@@ -6,7 +6,6 @@ use std::ops::{Range, RangeFrom, RangeFull, RangeTo};
 pub enum Token {
     Illegal,
     EOF,
-
     //Identifier and literals
     Ident(string),
     StringLiteral(String),
@@ -62,5 +61,40 @@ impl<'a> Tokens<'a> {
             start: 0,
             end: vec.len(),
         }
+    }
+}
+
+impl <'a> InputLength for Tokens<'a> {
+    #[inline]
+    fn input_len(&self) -> usize {
+        self.tok.len()
+    }
+}
+
+impl <'a> InputTake for Tokens<'a> {
+    #[inline]
+    fn take(&self, count: usize) -> Self {
+        Tokens {
+            tok: &self.tok[0..count],
+            start: 0,
+            end: count,
+        }
+    }
+
+
+    #[inline]
+    fn take_split(&self, count: usize) -> (Self, Self) {
+        let (prefix, suffix) = self.tok.split_at(count);
+        let first = Tokens {
+            tok: prefix,
+            start: 0,
+            end: prefix.len(),
+        };
+        let second = Tokens {
+            tok: suffix,
+            start: 0,
+            end: suffix.len(),
+        };
+        (second, first)
     }
 }
